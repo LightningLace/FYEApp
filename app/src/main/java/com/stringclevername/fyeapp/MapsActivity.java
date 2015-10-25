5a45d6f290a3ab0038859f9aec6ecb85be0ce60a
Colin Klein <colintklein@gmail.com>
2015-10-24 20:59:50 -0400

    Added "My Location" layer to map Added a default camera position on startup

--- app/src/main/java/com/stringclevername/fyeapp/MapsActivity.java
+++ app/src/main/java/com/stringclevername/fyeapp/MapsActivity.java
@@ -1,13 +1,16 @@
 package com.stringclevername.fyeapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -39,12 +42,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // Map will show user's location
+        // Check to see whether location permission has been granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Enable My Location layer, to show current location
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // TODO: Ask user to enable permission
+
+        }
+
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        LatLng hicks = new LatLng(42.289, -85.600);
+        LatLng hicks = new LatLng(42.289, -85.600); // Hicks Center
+        LatLng center = new LatLng(42.289714, -85.601228);  // ~center of campus
+
+        // Define default (main) camera position
+        CameraPosition main = new CameraPosition.Builder()
+                .target(center) // sets location coordinates to KCollege center
+                .zoom(18)       // sets zoom level
+                .bearing(270)    // sets orientation (West at top)
+                .build();        // creates a CameraPosition from the builder
+
         // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.addMarker(new MarkerOptions().position(hicks).title("Marker at Hicks Center"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(hicks));
+        mMap.addMarker(new MarkerOptions().position(hicks).title("Hicks Center"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(hicks));
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(main));
+
     }
 }
