ebba1f52efc6788d6fee08863b7e58facc634efb
Colin Klein <colintklein@gmail.com>
2015-10-24 21:55:34 -0400

    Added Google API client & methods, to use location data onConnect will generate and animate to a camera position using user's current location, or default to Kalamazoo College campus center.

--- app/src/main/java/com/stringclevername/fyeapp/MapsActivity.java
+++ app/src/main/java/com/stringclevername/fyeapp/MapsActivity.java
@@ -2,10 +2,15 @@ package com.stringclevername.fyeapp;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,9 +19,19 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+
+    // Define method to build API client
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Build API Client
+        buildGoogleApiClient();
+        // Connect to API client
+        mGoogleApiClient.connect();
+
     }
 
 
@@ -54,10 +75,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
         LatLng hicks = new LatLng(42.289, -85.600); // Hicks Center
         LatLng center = new LatLng(42.289714, -85.601228);  // ~center of campus
+
 
         // Define default (main) camera position
         CameraPosition main = new CameraPosition.Builder()
@@ -66,11 +88,55 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .bearing(270)    // sets orientation (West at top)
                 .build();        // creates a CameraPosition from the builder
 
-        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.addMarker(new MarkerOptions().position(hicks).title("Hicks Center"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(hicks));
-        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(main));
+        //mMap.moveCamera(CameraUpdateFactory.newCameraPosition(main));
+
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        // LatLng objects
+        LatLng newCenter;
+
+        // Get current location
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            // Store current location
+            newCenter = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            // Debug Toast
+            Toast success = Toast.makeText(this, "Debug: Success", Toast.LENGTH_SHORT);
+            success.show();
+        } else {
+            // Store K College center
+            newCenter = new LatLng(42.289714, -85.601228);  // ~center of campus
+            // Debug Toast
+            Toast error = Toast.makeText(this, "Debug: mLastLocation is null", Toast.LENGTH_SHORT);
+            error.show();
+        }
+        // Create CameraPosition to center on
+        CameraPosition main = new CameraPosition.Builder()
+                .target(newCenter)  // sets location coordinates to either current
+                                    // location or college center
+                .zoom(18)           // sets reasonable zoom level
+                .bearing(270)       // orient so West is up
+                .build();             // creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(main));
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
 }
