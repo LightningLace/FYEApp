5ecc5983951869795a1835694387145d367853e2
Ariah Lacey <ariahlacey@gmail.com>
2015-11-03 09:52:57 -0500

    added second activity

--- app/src/main/java/com/stringclevername/fyeapp/HomeScreen.java
+++ app/src/main/java/com/stringclevername/fyeapp/HomeScreen.java
@@ -1,5 +1,6 @@
 package com.stringclevername.fyeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,13 +12,12 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Switch;
 import android.widget.Toast;
 
 public class HomeScreen extends AppCompatActivity {
-
-    private RadioGroup radioSexGroup;
-    private RadioButton radioSexButton;
+    public final static String EXTRA_NAME = "com.stringclevername.fyeapp.MESSAGE";
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
     private Button btnDisplay;
 
     @Override
@@ -31,7 +31,7 @@ public class HomeScreen extends AppCompatActivity {
 
     public void addListenerOnButton() {
 
-        radioSexGroup = (RadioGroup) findViewById(R.id.radioStudentType);
+        radioGroup = (RadioGroup) findViewById(R.id.radioStudentType);
         btnDisplay = (Button) findViewById(R.id.button1);
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
@@ -40,16 +40,25 @@ public class HomeScreen extends AppCompatActivity {
             public void onClick(View v) {
 
                 // get selected radio button from radioGroup
-                int selectedId = radioSexGroup.getCheckedRadioButtonId();
+                int selectedId = radioGroup.getCheckedRadioButtonId();
 
                 // find the radiobutton by returned id
-                radioSexButton = (RadioButton) findViewById(selectedId);
+                radioButton = (RadioButton) findViewById(selectedId);
 
-                Toast.makeText(HomeScreen.this,radioSexButton.getText(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(HomeScreen.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
 
             }
 
         });
 
+    }
+
+    /** Called when the user clicks the Go! button */
+    public void login(View view) {
+        Intent intent = new Intent(this, HomeScreen2.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String studentName = editText.getText().toString();
+        intent.putExtra(EXTRA_NAME, studentName);
+        startActivity(intent);
     }
 }
